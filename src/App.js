886528e5e13e3c2aefdf9d0a54904c8161faea74
Rachel Borgwald <rborgwald2885@gmail.com>
2019-04-23 19:54:13 -0500

    #36 - components, props

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 import React, { Component } from 'react';
 import './App.css';
+import Person from './Person/Person';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <h1>Hi, I'm a React App</h1>
+        <Person name="Rachel" age="33"/>
+        <Person name="Chase" age="35">My hobbies: Racing</Person>
+        <Person name="Penny" age="15"/>
       </div>
     );
+    // Above jsx compiles to code below
+    // return React.createElement('div', {className: 'App'}, React.createElement('h1',null, 'Hi, I\'m a React App!!!'));
   }
 }
 
