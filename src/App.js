cf26233111ee246704866116a6db05a27c7c11e6
Rachel Borgwald <trkb3@unigroupinc.com>
2019-04-25 14:58:52 -0500

    #47 Passing method references; Styling options

--- src/App.js
+++ src/App.js
@@ -12,22 +12,52 @@ class App extends Component {
     otherState: 'some other value',
   };
 
-  switchNameHandler = () => {
+  switchNameHandler = (newName) => {
     this.setState({persons:  [
         {name: 'Rachel', age: 33},
-        {name: 'Jonathan', age: 35, hobby: 'Music'},
+        {name: newName, age: 35, hobby: 'Music'},
+        {name: 'Penny', age: 26}
+      ]});
+  };
+
+  nameChangedHandler = (event) => {
+    this.setState({persons:  [
+        {name: 'Rachel', age: 33},
+        {name: event.target.value, age: 35, hobby: 'Music'},
         {name: 'Penny', age: 26}
       ]});
   };
 
   render() {
+
+    const style = {
+      backgroundColor: 'white',
+      font: 'inherit',
+      border: '1px solid blue',
+      padding: '8px',
+      cursor: 'pointer',
+    };
+
     return (
       <div className="App">
         <h1>Hi, I'm a React App</h1>
-        <button onClick={this.switchNameHandler}>Switch Name</button>
-        <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
-        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>My hobby: {this.state.persons[1].hobby}</Person>
-        <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
+        <button
+          style={style}
+          onClick={() => this.switchNameHandler('Chase!!')}>Switch Name</button>
+        <Person
+          name={this.state.persons[0].name}
+          age={this.state.persons[0].age}/>
+        <Person
+          name={this.state.persons[1].name}
+          age={this.state.persons[1].age}
+          click={this.switchNameHandler.bind(this, 'Foobar!')}
+          changed={this.nameChangedHandler}
+        >
+          My hobby: {this.state.persons[1].hobby}
+        </Person>
+        <Person
+          name={this.state.persons[2].name}
+          age={this.state.persons[2].age}/>
       </div>
     );
     // Above jsx compiles to code below
