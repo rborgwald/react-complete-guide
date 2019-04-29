19120b647158b2cbb3578680af0ebe60575712b5
Rachel Borgwald <trkb3@unigroupinc.com>
2019-04-29 08:05:10 -0500

    #79 Debugging and ErrorBoundary

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import classes from './App.module.css';
 import Person from './Person/Person';
+import ErrorBoundary from './ErrorBoundary/ErrorBoundary';
 
 class App extends Component {
   state = {
@@ -42,13 +43,12 @@ class App extends Component {
     if(this.state.showPersons) {
       persons = (<div>
         {this.state.persons.map((p, idx) =>
-          <Person
-            key={p.id}
+          <ErrorBoundary key={p.id}><Person
             name={p.name}
             age={p.age}
             click={() => this.deletePersonHandler(idx)}
             changed={(event) => this.nameChangedHandler(event, p.id)}
-          />)
+          /></ErrorBoundary>)
         }
       </div>);
       btnClass = classes.Red;
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -3,6 +3,11 @@ import classes from './Person.module.css';
 
 const person = props => {
 
+  const random = Math.random();
+  if (random > 0.7) {
+    throw new Error('Something went wrong');
+  }
+
   return (
     <div className={classes.Person}>
       <p onClick={props.click}>
