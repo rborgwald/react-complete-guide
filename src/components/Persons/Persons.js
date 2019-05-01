1e42cdaec654027e932fa3d305fac5a44bf2a7c2
Rachel Borgwald <trkb3@unigroupinc.com>
2019-05-01 15:35:03 -0500

    Lifecycle methods - partially complete

--- src/components/Persons/Persons.js
+++ src/components/Persons/Persons.js
@@ -1,15 +1,40 @@
-import React from 'react';
+import React, {Component} from 'react';
 import Person from './Person/Person';
 
-const persons = (props) =>
-  props.persons.map((p, idx) =>
-    <Person
-      key={p.id}
-      name={p.name}
-      age={p.age}
-      click={() => props.clicked(idx)}
-      changed={(event) => props.changed(event, p.id)}
-    />);
+class Persons extends Component {
+  // static getDerivedStateFromProps(props, state) {
+  //   console.log('[Persons.js] getDerivedStateFromProps');
+  //   return state;
+  // }
 
-export default persons;
+  shouldComponentUpdate(nextProps, nextState) {
+    console.log('[Persons.js] shouldComponentUpdate');
+    return true;
+  }
+
+  getSnapshotBeforeUpdate(prevProps, prevState) {
+    console.log('[Persons.js] getSnapshotBeforeUpdate');
+    return {message: 'Snapshot!'};
+  }
+
+  componentDidUpdate(prevProps, prevState, snapshot) {
+    console.log('[Persons.js] componentDidUpdate', snapshot);
+  }
+
+  render() {
+    console.log('[Persons.js] rendering...');
+
+    return this.props.persons.map((p, idx) =>
+      <Person
+        key={p.id}
+        name={p.name}
+        age={p.age}
+        click={() => this.props.clicked(idx)}
+        changed={(event) => this.props.changed(event, p.id)}
+      />);
+  }
+};
+
+
+export default Persons;
 
--- src/containers/App.js
+++ src/containers/App.js
@@ -4,6 +4,11 @@ import Persons from '../components/Persons/Persons';
 import Cockpit from '../components/Cockpit/Cockpit';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    console.log('[App.js] constructor');
+  }
+
   state = {
     persons: [
       {id: 1, name: 'Rachel', age: 33},
@@ -13,6 +18,15 @@ class App extends Component {
     otherState: 'some other value',
     showPersons: false,
   };
+
+  static getDerivedStateFromProps(props, state) {
+    console.log('[App.js] getDerivedStateFromProps', props);
+    return state;
+  }
+
+  componentDidMount() {
+    console.log('[App.js] componentDidMount');
+  }
 
   nameChangedHandler = (event, id) => {
     const personIndex = this.state.persons.findIndex(p => p.id === id);
@@ -36,7 +50,7 @@ class App extends Component {
   };
 
   render() {
-
+    console.log('[App.js] render');
     let persons = null;
 
     if(this.state.showPersons) {
