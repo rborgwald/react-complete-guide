d83bd11661006fc61ca6e3591c43aac801d7497f
Rachel Borgwald <rborgwald2885@gmail.com>
2019-05-02 19:50:23 -0500

    #104 HOC examples

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -1,20 +1,24 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import classes from './Person.module.css';
+import WithClass from '../../../hoc/WithClass';
 
 class Person extends Component {
   render() {
     console.log('[Person.js] rendering...');
     return (
-      <div className={classes.Person}>
+      <WithClass classes={classes.Person}>
         <p onClick={this.props.click}>
           I'm {this.props.name} and I am {this.props.age} years old!
         </p>
         <p>{this.props.children}</p>
-        <input type="text" onChange={this.props.changed} value={this.props.name}/>
-      </div>
+        <input
+          type="text"
+          onChange={this.props.changed}
+          value={this.props.name}
+        />
+      </WithClass>
     );
   }
-
-};
+}
 
 export default Person;
--- src/containers/App.js
+++ src/containers/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import classes from './App.module.css';
 import Persons from '../components/Persons/Persons';
 import Cockpit from '../components/Cockpit/Cockpit';
+import WithClass from '../hoc/WithClass';
 
 class App extends Component {
   constructor(props) {
@@ -70,7 +71,7 @@ class App extends Component {
     }
 
     return (
-      <div className={classes.App}>
+      <WithClass classes={classes.App}>
         <button onClick={() => { this.setState({showCockpit: false});}}>Remove Cockpit</button>
         {this.state.showCockpit && <Cockpit
           title={this.props.appTitle}
@@ -78,7 +79,7 @@ class App extends Component {
           personsLength={this.state.persons.length}
           clicked={this.togglePersonsHandler}/>}
         {persons}
-      </div>
+      </WithClass>
     );
   }
 }
